19e7045ae786961dc0b2d15d2206bc6ee5d8a19e
MihaelHoli <mholi@student.unipu.hr>
2024-03-12 17:11:00 +0100

    uri

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ document.getElementById('authorizeButton').addEventListener('click', function() 
                 'Authorization': 'Basic ' + btoa(`23RT5X` + ':' + `579d36577af143adf1e504fa3ccb47aa`),
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
-            body: 'grant_type=authorization_code&code=' + code + '&redirect_uri=http://localhost:8553'
+            body: 'grant_type=https://www.fitbit.com/oauth2/authorize&code=' + code + '&redirect_uri=https://mihael-tin-banda.github.io'
         })
         .then(response => response.json())
         .then(data => {
@@ -32,6 +32,6 @@ document.getElementById('authorizeButton').addEventListener('click', function() 
             .catch(error => console.error('Error:', error));
         });
     } else {
-        window.location.href = `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23RT5X&scope=activity&redirect_uri=http://localhost:8553&state=2w6v1l1h502m0o4d006t242s095y6w2g`;
+        window.location.href = `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23RT5X&scope=cardio_fitness location oxygen_saturation temperature electrocardiogram weight sleep settings nutrition activity heartrate profile social respiratory_rate&redirect_uri=https://mihael-tin-banda.github.io&state=2w6v1l1h502m0o4d006t242s095y6w2g`;
     }
 }); // Add closing parenthesis here
