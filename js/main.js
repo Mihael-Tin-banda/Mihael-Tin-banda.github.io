d1fa4bc11088c197a99a84190ea756eb4c40ad83
MihaelHoli <mholi@student.unipu.hr>
2024-03-14 15:25:22 +0100

    access token store

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+unction getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+window.onload = function() {
+  var accessToken = getParameterByName('access_token');
+  if (accessToken) {
+      // Save the access token somewhere (e.g., in session storage)
+      sessionStorage.setItem('access_token', accessToken);
+  }
+};
+
 var config = {
   web: {
     client_id:
